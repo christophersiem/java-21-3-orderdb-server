a2b64d0ec38e5eddb87e01b29c9b2b5b643d9951
Stefan Lautner <stefan.lautner@highpace.de>
2021-07-28 15:32:32 +0200

    add mocked OrderServiceTest's

---
+++ src/test/java/de/neuefische/java213orderdbserver/controller/OrderControllerTest.java
@@ -0,0 +1,35 @@
+package de.neuefische.java213orderdbserver.controller;
+
+import de.neuefische.java213orderdbserver.model.Order;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import javax.annotation.Resource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+class OrderControllerTest {
+
+    @Resource
+    private TestRestTemplate testRestTemplate;
+
+    private final static String BASE_URL = "http://localhost:8080";
+
+    @Test
+    public void testRest() {
+        String url = String.format("%s/order/all", BASE_URL);
+        ResponseEntity<Order[]> orderResponseEntity = testRestTemplate.getForEntity(url, Order[].class);
+
+        assertEquals(HttpStatus.OK, orderResponseEntity.getStatusCode());
+
+        Order[] orderArray = orderResponseEntity.getBody();
+        assertNotNull(orderArray);
+        assertEquals(0, orderArray.length);
+    }
+}
+
