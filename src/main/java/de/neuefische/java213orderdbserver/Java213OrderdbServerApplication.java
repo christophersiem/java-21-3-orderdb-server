974fc427a950109c46dbc71e102fe4aed0b5f83e
christophersiem <christopher.siem@gmail.com>
2021-07-28 11:04:00 +0200

    Add .DS_Store to gitignore

---
+++ src/main/java/de/neuefische/java213orderdbserver/Java213OrderdbServerApplication.java
@@ -0,0 +1,13 @@
+package de.neuefische.java213orderdbserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java213OrderdbServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Java213OrderdbServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/neuefische/java213orderdbserver/Java213OrderdbServerApplicationTests.java
@@ -0,0 +1,13 @@
+package de.neuefische.java213orderdbserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Java213OrderdbServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
